688e5ba1ec2c89799b24aa355161e0104232bbf3
shubhkarmansohi <sss669@mail.usask.ca>
2021-03-29 23:31:18 -0600

    Work Done, add skills later

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React from 'react'
 import Greetings from "./Home";
 import About from "./About";
 import Skills from "./Skills"
+import Footer from "./Footer";
 
 function App(){
     return(<>
         <Greetings/>
         <About/>
         <Skills/>
+        <Footer/>
     </>);
 }
 
